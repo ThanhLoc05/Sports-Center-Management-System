9ed5d516728bba6b9ffb71f7bcc54be9b2717bb5
m1nhkh0a <minhkhoact138@icloud.com>
2026-09-19 13:49:45 +0700

    feat: them cac file khoi tao cho back end

---
+++ src/test/java/com/culturalhouse/sportcenter/SportCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.culturalhouse.sportcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
